b0d0f829c90a5417bf0f4d7e2c7913049232c7ce
Eronmonsele oaikhina <eronoaik@gmail.com>
2024-10-04 12:40:43 +0100

    pushing

--- src/OnchainProviders.tsx
+++ src/OnchainProviders.tsx
@@ -14,6 +14,12 @@ import {
 } from "@rainbow-me/rainbowkit/wallets";
 import "@coinbase/onchainkit/styles.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import {
+  Web3AuthInnerContext,
+  Web3AuthProvider,
+} from "@web3auth/modal-react-hooks";
+import { WalletServicesProvider } from "@web3auth/wallet-services-plugin-react-hooks";
+import web3AuthContextConfig from "./utils/services/web3authContext";
 
 type Props = { children: ReactNode };
 
@@ -49,14 +55,18 @@ function OnchainProviders({ children }: Props) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey={process.env.PUBLIC_ONCHAINKIT_API_KEY}
-          chain={baseSepolia}
-        >
-          <RainbowKitProvider modalSize="compact">
-            {children}
-          </RainbowKitProvider>
-        </OnchainKitProvider>
+        <Web3AuthProvider config={web3AuthContextConfig}>
+          <WalletServicesProvider context={Web3AuthInnerContext}>
+            <OnchainKitProvider
+              apiKey={process.env.PUBLIC_ONCHAINKIT_API_KEY}
+              chain={baseSepolia}
+            >
+              <RainbowKitProvider modalSize="compact">
+                {children}
+              </RainbowKitProvider>
+            </OnchainKitProvider>
+          </WalletServicesProvider>
+        </Web3AuthProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
---
+++ src/utils/services/web3authContext.ts
@@ -0,0 +1,48 @@
+import { UX_MODE } from "@toruslabs/openlogin-utils";
+import { WEB3AUTH_NETWORK } from "@web3auth/base";
+import { getDefaultExternalAdapters } from "@web3auth/default-evm-adapter";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { Web3AuthOptions } from "@web3auth/modal";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { WalletServicesPlugin } from "@web3auth/wallet-services-plugin";
+
+//import { chain } from "./config/chainConfig";
+import {chain} from '../config/chain'
+
+const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
+
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: {
+    chainConfig: chain.base,
+  },
+});
+
+const web3AuthOptions: Web3AuthOptions = {
+  chainConfig: chain.ethereum,
+  clientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+  privateKeyProvider,
+};
+
+const openloginAdapter = new OpenloginAdapter({
+  loginSettings: {
+    mfaLevel: "optional",
+  },
+  adapterSettings: {
+    uxMode: UX_MODE.REDIRECT, // "redirect" | "popup"
+  },
+});
+
+const walletServicesPlugin = new WalletServicesPlugin({
+  wsEmbedOpts: {},
+  walletInitOptions: { whiteLabel: { showWidgetButton: true, buttonPosition: "bottom-right" } },
+});
+
+//const adapters = await getDefaultExternalAdapters({ options: web3AuthOptions });
+
+const web3AuthContextConfig = {
+  web3AuthOptions,
+  plugins: [walletServicesPlugin],
+};
+
+export default web3AuthContextConfig;
